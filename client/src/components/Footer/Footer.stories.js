57233f9fdab34749473046b459c5d916fded9526
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-10-29 17:43:13 +0200

    add footer

---
+++ client/src/components/Footer/Footer.stories.js
@@ -0,0 +1,14 @@
+import Footer from "./Footer";
+
+export default {
+  name:"Footer",
+  component: Footer
+}
+
+const Template = (args) => <Footer {...args} />;
+
+export const First = Template.bind({})
+
+First.args = {
+
+}
