093956d68b61add17984ab10205a2f736cea8d57
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-05 14:42:27 +0100

    change layout Submit Page

--- client/src/features/submitPage/SubmitPage.js
+++ client/src/features/submitPage/SubmitPage.js
@@ -1,23 +1,36 @@
 import Input from "../../components/Input/Input";
-import EmojiText from "../../components/EmojiText/EmojiText"
-import styles from "./SubmitPage.module.css"
-import ResponsiveContainer from "../../components/ResponsiveContainer/ResponsiveContainer"
-import Button from "../../components/Button/Button"
-import Selector from "../../components/Selector/Selector"
+import EmojiText from "../../components/EmojiText/EmojiText";
+import styles from "./SubmitPage.module.css";
+import ResponsiveContainer from "../../components/ResponsiveContainer/ResponsiveContainer";
+import Button from "../../components/Button/Button";
+import Selector from "../../components/Selector/Selector";
 
 export default function SubmitPage() {
   return (
     <ResponsiveContainer>
       <div className={styles.inputContainer}>
-        <EmojiText emoji={"👋"} text={"Dein Name"}/>
-        <Input placeholder={"max mustermann"} name={"name"}/>
-        <EmojiText emoji={"✉️"} text={"Deine E-Mail Adresse"} htmlFor={"email"}/>
-        <Input placeholder={"example@test.de"} name={"email"}/>
-        <EmojiText emoji={"📞"} text={"Deine Telefonnummer"}  htmlFor={"phoneNumber"}/>
-        <Input placeholder={"01234 56789"} name={"phoneNumber"}/>  
+        <EmojiText emoji={"👋"} text={"Dein Name"} />
+        <Input placeholder={"max mustermann"} name={"name"} />
+        <EmojiText
+          emoji={"✉️"}
+          text={"Deine E-Mail Adresse"}
+          htmlFor={"email"}
+        />
+        <Input placeholder={"example@test.de"} name={"email"} />
+        <EmojiText
+          emoji={"📞"}
+          text={"Deine Telefonnummer"}
+          htmlFor={"phoneNumber"}
+        />
+        <Input placeholder={"01234 56789"} name={"phoneNumber"} />
       </div>
-      <Selector emoji={"🖋️"} text={"Datenschutz erklärung gelesn und Akzeptiert"}/>
-      <Button text={"Absenden 📬"}/>
+      <div className={styles.container}>
+        <Selector
+          emoji={"🖋️"}
+          text={"Datenschutzerklärung gelesen und Akzeptiert"}
+        />
+        <Button text={"Absenden 📬"} />
+      </div>
     </ResponsiveContainer>
   );
 }
