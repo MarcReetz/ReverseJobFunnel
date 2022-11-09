c1c2f2cb79ecfa43d50c3621605e2b20d52a6ae5
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-09 16:45:03 +0100

    start startPage

---
+++ client/src/features/startPage/StartPage.js
@@ -0,0 +1,10 @@
+import ResponsiveContainer from "../../components/ResponsiveContainer/ResponsiveContainer"
+
+export default function StartPage () {
+
+  return (
+    <ResponsiveContainer>
+      
+    </ResponsiveContainer>
+  )
+} 
