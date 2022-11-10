ac0f991831f070a9318e207d1d78218a44be3f41
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-10 21:41:56 +0100

    add center layout element

---
+++ client/src/components/Center/CenterX.stories.js
@@ -0,0 +1,14 @@
+import CenterX from "./CenterX";
+
+export default {
+  name:"CenterX",
+  component: CenterX
+}
+
+const Template = (args) => <CenterX><p>Test</p></CenterX>
+
+export const First = Template.bind()
+
+First.args = {
+
+}
