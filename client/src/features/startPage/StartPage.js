6714010af2304cd8320abb2524cf18867290124b
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-10 17:23:37 +0100

    add startPage

--- client/src/features/startPage/StartPage.js
+++ client/src/features/startPage/StartPage.js
@@ -1,10 +1,32 @@
 import ResponsiveContainer from "../../components/ResponsiveContainer/ResponsiveContainer"
+import SubTitle from "../../components/SubTitle/SubTitle"
+import Title from "../../components/Title/Title"
+import Button from "../../components/Button/Button"
 
 export default function StartPage () {
 
   return (
     <ResponsiveContainer>
-      
+      <SubTitle>
+        Einen neuen Mitarbeiter in nur 2 Minuten -<br/>
+        ohne Headhunter ✨
+      </SubTitle>
+      <Title>
+        Moin ich bin<br/>
+        <strong>Marc Reetz</strong><br/>
+        und ich könnte ihr Team verstärken als <br/>
+        <strong>Software Developer</strong>
+      </Title>
+      <SubTitle>
+        Beginn: ab sofort möglich <br/>
+        Ort: Bremen und Umgebung / <br/>
+        remote weltweit
+      </SubTitle>
+
+      <Button text={"Finde mehr über mich heraus 🎉"}/>
+      <SubTitle>
+        <strong>Ein Marc für ihr Team</strong>
+      </SubTitle>
     </ResponsiveContainer>
   )
 } 
