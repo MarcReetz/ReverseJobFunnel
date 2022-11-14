f2d94ec8fdcc5e0443b087f84203a92f0cd6da0e
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-14 15:49:38 +0100

    add GitLink Element

---
+++ client/src/components/GitLink/GitLink.js
@@ -0,0 +1,12 @@
+import styles from "./GitLink.module.css";
+
+export default function GitLink() {
+  return (
+    <a className={styles.link}href={"https://github.com/MarcReetz/ReverseJobFunnel"}>
+      <div className={styles.container}>
+        <img src={"GitHub-Mark-64px.png"} alt={"Githublink"} />
+        <p>Find the code to this  <br/> page on my <strong>GitHub</strong></p>
+      </div>
+    </a>
+  );
+}
