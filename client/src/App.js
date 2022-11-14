479ff9e66261ebcaba78520cfda9ab11501279f9
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-14 18:06:56 +0100

    add routes

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,20 @@ import React from 'react';
 import { Routes,Route } from "react-router-dom";
 import Frame from './features/frame/Frame';
 import StartPage from './features/startPage/StartPage'
+import SecondPage from './features/secondPage/SecondPage'
+import ThirdPage from './features/thirdPage/ThirdPage'
+import BenefitsPage from "./features/benefitsPage/BenefitsPage"
+import SubmitPage from './features/submitPage/SubmitPage'
 
 function App() {
   return (
      <Routes>
       <Route path={'/'} element={<Frame/>}>
         <Route path={'start'} element={<StartPage/>}/>
+        <Route path={'skills'} element={<SecondPage/>}/>
+        <Route path={'softskills'} element={<ThirdPage/>}/>
+        <Route path={'benefits'} element={<BenefitsPage/>}/>
+        <Route path={'submit'} element={<SubmitPage/>}/>
       </Route>
      </Routes>
   );
