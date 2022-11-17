137de602b2fdd03f2040324da422f4553fe7de93
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-17 22:29:39 +0100

    add concentrationQuizPage

---
+++ client/src/features/concentrationQuizPage/ConcenctrationQuizPage.stories.js
@@ -0,0 +1,15 @@
+import ConcentrationQuizPage from "./ConcentrationQuizPage";
+
+export default {
+  name: 'ConcentrationQuizPage',
+  component: ConcentrationQuizPage
+}
+
+const Template = (args) => <ConcentrationQuizPage/>
+
+export const First = Template.bind()
+
+First.args = {
+  
+}
+
---
+++ client/src/features/concentrationQuizPage/ConcentrationQuizPage.js
@@ -0,0 +1,25 @@
+import ResponsiveContainer from '../../components/ResponsiveContainer/ResponsiveContainer'
+import Title from "../../components/Title/Title";
+import QuizButton from "../../components/QuizButton/QuizButton";
+import QuizButtonContainer from "../../components/QuizButtonContainer/QuizButtonContainer";
+import { useNavigate } from "react-router-dom";
+import SubTitle from '../../components/SubTitle/SubTitle';
+
+export default function ConcentrationQuizPage () {
+  const navigate = useNavigate();
+
+  return (
+    <ResponsiveContainer>
+      <Title>
+        Die Abwechslungs machts! 
+      </Title>
+      <SubTitle>
+      Neben Guten Team Work bekommt man bei euch im Team auch genügent zeit um alleine oder beim Pair Programming <strong>Konzentriet</strong> zu arbeiten 
+      </SubTitle>
+      <QuizButtonContainer>
+        <QuizButton emoji="🔊" text={"Wie du brauchst ruhe zum arbeiten !?"} />
+        <QuizButton emoji="🧠" text={"Natürlich"} onClick={() => navigate()}/>
+      </QuizButtonContainer>
+    </ResponsiveContainer>
+  )
+}
