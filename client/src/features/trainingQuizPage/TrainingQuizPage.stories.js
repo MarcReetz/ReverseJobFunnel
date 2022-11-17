b9d31aec86c88450c1c576175f7afa2598f97aee
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-17 17:38:51 +0100

    add trainingQuizPage

---
+++ client/src/features/trainingQuizPage/TrainingQuizPage.stories.js
@@ -0,0 +1,15 @@
+import TrainingQuizPage from "./TrainingQuizPage";
+
+export default {
+  name: 'TrainingQuizPage',
+  component: TrainingQuizPage
+}
+
+const Template = (args) => <TrainingQuizPage/>
+
+export const First = Template.bind()
+
+First.args = {
+  
+}
+
