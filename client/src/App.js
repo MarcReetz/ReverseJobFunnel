a9968154909e8557ff8016a8df30be3f473136c4
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-18 14:01:01 +0100

    add path skill quiz fail

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,46 @@
-import React from 'react';
-import { Routes,Route } from "react-router-dom";
-import Frame from './features/frame/Frame';
-import StartPage from './features/startPage/StartPage'
-import SecondPage from './features/secondPage/SecondPage'
-import ThirdPage from './features/thirdPage/ThirdPage'
-import BenefitsPage from "./features/benefitsPage/BenefitsPage"
-import SubmitPage from './features/submitPage/SubmitPage'
-import SkillQuizPage from './features/skillQuizPage/SkillQuizPage';
-import TrainingQuizPage from './features/trainingQuizPage/TrainingQuizPage';
-import TeamQuizPage from './features/teamQuizPage/TeamQuizPage';
-import ConcentrationQuizPage from './features/concentrationQuizPage/ConcentrationQuizPage';
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Frame from "./features/frame/Frame";
+import StartPage from "./features/startPage/StartPage";
+import SecondPage from "./features/secondPage/SecondPage";
+import ThirdPage from "./features/thirdPage/ThirdPage";
+import BenefitsPage from "./features/benefitsPage/BenefitsPage";
+import SubmitPage from "./features/submitPage/SubmitPage";
+import SkillQuizPage from "./features/skillQuizPage/SkillQuizPage";
+import TrainingQuizPage from "./features/trainingQuizPage/TrainingQuizPage";
+import TeamQuizPage from "./features/teamQuizPage/TeamQuizPage";
+import FailPage from "./";
+import ConcentrationQuizPage from "./features/concentrationQuizPage/ConcentrationQuizPage";
 
 function App() {
   return (
-     <Routes>
-      <Route path={'/'} element={<Frame/>}>
-        <Route path={'start'} element={<StartPage/>}/>
-        <Route path={'skills'} element={<SecondPage/>}/>
-        <Route path={'softskills'} element={<ThirdPage/>}/>
-        <Route path={'benefits'} element={<BenefitsPage/>}/>
-        <Route path={'submit'} element={<SubmitPage/>}/>
-        <Route path={'skill-quiz'} element={<SkillQuizPage/>}/>
-        <Route path={'training-quiz'} element={<TrainingQuizPage/>}/>
-        <Route path={'team-quiz'} element={<TeamQuizPage/>}/>
-        <Route path={'concentration-quiz'} element={<ConcentrationQuizPage/>}/>
+    <Routes>
+      <Route path={"/"} element={<Frame />}>
+        <Route path={"start"} element={<StartPage />} />
+        <Route path={"skills"} element={<SecondPage />} />
+        <Route path={"softskills"} element={<ThirdPage />} />
+        <Route path={"benefits"} element={<BenefitsPage />} />
+        <Route path={"submit"} element={<SubmitPage />} />
+        <Route path={"skill-quiz"} element={<SkillQuizPage />} />
+        <Route
+          path={"skill-quiz-fail"}
+          element={
+            <FailPage
+              text={
+                'ich kann dir und deinen Team leider nicht viele abschlüsse anbieten. Ich habe einige Kurse auf codecademy abgeschlossen. Ich besitze Relevante Fähikeiten in der Front- und Back-end etwicklung schaue dir dazu gerne mein verlinktes Github profile an!'
+              }
+              navigate={'/skill-quiz'}
+            />
+          }
+        />
+        <Route path={"training-quiz"} element={<TrainingQuizPage />} />
+        <Route path={"team-quiz"} element={<TeamQuizPage />} />
+        <Route
+          path={"concentration-quiz"}
+          element={<ConcentrationQuizPage />}
+        />
       </Route>
-     </Routes>
+    </Routes>
   );
 }
 
