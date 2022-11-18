97ed9db95875cf54f45f4c69a39bebafb742c3aa
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-18 20:36:41 +0100

    start backend server

---
+++ server.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/cors"
+)
+
+func main() {
+	r := chi.NewRouter()
+	r.Use(middleware.Logger)
+
+	r.Use(cors.Handler(cors.Options{
+		// AllowedOrigins:   []string{"https://foo.com"}, // Use this to allow specific origin hosts
+		AllowedOrigins: []string{"https://*", "http://*"},
+		// AllowOriginFunc:  func(r *http.Request, origin string) bool { return true },
+		AllowedMethods:   []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Accept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300, // Maximum value not ignored by any of major browsers
+	}))
+
+	r.Route("/api", func(r chi.Router) {
+		r.Post("/signup", signup)
+		r.Get("/mail/{apiID}", mailSignup)
+	})
+
+}
+
+func signup(w http.ResponseWriter, r *http.Request) {
+
+}
+
+func mailSignup(w http.ResponseWriter, r *http.Request) {
+
+}
