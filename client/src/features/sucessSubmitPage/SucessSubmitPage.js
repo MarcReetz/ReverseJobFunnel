4796a5f0bbae2436009e7d01e735d2ed2ed74971
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-23 17:14:47 +0100

    add sucess Page

---
+++ client/src/features/sucessSubmitPage/SucessSubmitPage.js
@@ -0,0 +1,15 @@
+import ResponsiveContainer from "../../components/ResponsiveContainer/ResponsiveContainer";
+import SubTitle from "../../components/SubTitle/SubTitle";
+import Title from "../../components/Title/Title";
+import TextLeft from "../../components/TextLeft/TextLeft"
+
+export default function SucessSubmitPage () {
+  return (
+    <ResponsiveContainer>
+      <Title>Deine Daten sind sicher Angekommen!</Title>
+      <SubTitle>Bitte <strong>Bestätige</strong> deine <strong>E-mail Adresse</strong></SubTitle>
+      <SubTitle>Die E-mail zur Bestätigung würde an deine E-mail versendet. Absender ist <i>automation@marc-reetz.de</i> Eventuel must du in deinen Spam Ordner Schauen. Ich hoffe wir Hören uns bald 👋!</SubTitle>
+      <TextLeft>Bestätigst du nicht deine Email Adresse werden alle daten von dir automatisch nach 48-49H gelöscht. Ich nehme erst Kontakt zu dir auf wenn du deine E-mail Adresse bestätigst hast.</TextLeft>
+    </ResponsiveContainer>
+  )
+}
---
+++ client/src/features/sucessSubmitPage/SucessSubmitPage.stories.js
@@ -0,0 +1,14 @@
+import SucessSubmitPage from "./SucessSubmitPage";
+
+export default {
+  name:"SuccessSubmitPage",
+  component:SucessSubmitPage
+}
+
+const Template = (args) => <SucessSubmitPage/>
+
+export const First = Template.bind()
+
+First.args = {
+  
+}
