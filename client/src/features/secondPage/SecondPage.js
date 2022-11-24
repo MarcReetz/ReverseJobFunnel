72e668331d701a93ec7b774cdd00fd6c83311e34
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-11-24 17:32:02 +0100

    add Content to secondPage

--- client/src/features/secondPage/SecondPage.js
+++ client/src/features/secondPage/SecondPage.js
@@ -14,7 +14,12 @@ export default function SecondPage () {
     <ResponsiveContainer>
       <Title><strong>Ein paar worte zu mir</strong></Title>
       <EmojiTextTable>
-        <EmojiTextTableElement emoji={"💻"}>Ein paar dinge mit dehnen ich Arbeite: Go, Javascript, Css3, Html5</EmojiTextTableElement>
+      <EmojiTextTableElement emoji={"🙋🏻‍♂️"}>Die Basics: Ich bin Marc Reetz 24 Jahre alt und Wohne im Schönen Bremen.</EmojiTextTableElement>
+      <EmojiTextTableElement emoji={"💻"}>Ich behersche folgende Programmier Sprachen: Go, Javascript, Typescritpt.</EmojiTextTableElement>
+      <EmojiTextTableElement emoji={"👔"}>In Meinen Berufsleben könnte ich bereits 1,5 Jahre Nachweisbare erfahrung in der Softwarentwicklung, insbesondere mit Java,Css3,Html5,Javascript sammeln.</EmojiTextTableElement>
+      <EmojiTextTableElement emoji={"🧑🏻‍💻"}>In Letzter Zeit habe ich mich Intensiv mit React und dem dazugehörigen Eco System (ReactRouter,I18n,Redux,storybook...) Auseinander gesetzt.</EmojiTextTableElement>
+      <EmojiTextTableElement emoji={"🌈"}>Natürlich kommt auch das design nicht zu kurz. Ich behersche: HTML5 und CSS3.</EmojiTextTableElement>
+      <EmojiTextTableElement emoji={"📍"}>Diese Seite verwendet einen PRO Stack 😉 (PostgreSQL,React,Go). Schaue dir den Code auf Github an!</EmojiTextTableElement>
       </EmojiTextTable>
       <CenterX>
         <Button text={"Cool was hast du noch zu bieten"} onClick={() => navigate('/softskills')}/>
