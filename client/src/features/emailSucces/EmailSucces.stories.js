21dfcb4e5e8fdff2bcf5f063965025a1f705d50b
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-12-02 16:06:49 +0100

    add email sucess page

---
+++ client/src/features/emailSucces/EmailSucces.stories.js
@@ -0,0 +1,14 @@
+import EmailSucces from "./EmailSucces";
+
+export default {
+  name: "EmailSucces",
+  component: EmailSucces
+}
+
+const Template = (args) => <EmailSucces {...args}/>
+
+export const First = Template.bind()
+
+First.args = {
+
+}
