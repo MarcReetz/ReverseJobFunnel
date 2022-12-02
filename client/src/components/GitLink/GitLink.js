1ed4980e8c32b8bb6f137a872a06a8ddfbfdebcf
MarcReetz <8797067+MarcReetz@users.noreply.github.com>
2022-12-02 17:07:02 +0100

    minor changes to frontend text

--- client/src/components/GitLink/GitLink.js
+++ client/src/components/GitLink/GitLink.js
@@ -5,7 +5,7 @@ export default function GitLink() {
     <a className={styles.link}href={"https://github.com/MarcReetz/ReverseJobFunnel"} target={"_blank"} rel={"noreferrer"}>
       <div className={styles.container}>
         <img src={"GitHub-Mark-64px.png"} alt={"Githublink"} />
-        <p>Find the code to this  <br/> page on my <strong>GitHub</strong></p>
+        <p>Finde den Code zu <br/> dieser Seite auf <strong>GitHub</strong></p>
       </div>
     </a>
   );
